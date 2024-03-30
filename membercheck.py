59ac0660e83d80548c856646c5d5acaf2d7ff200
davexxx <davexxx@163.com>
2024-03-30 22:25:47 +0800

    加入会员资格检测

--- membercheck.py
+++ membercheck.py
@@ -43,7 +43,8 @@ class membercheck(Plugin):
             self.db_password = self.config.get("db_password","")
             self.db_name = self.config.get("db_name", "")
             self.credit_prefix = self.config.get("credit_prefix", "余额")
-
+            self.reg_url = self.config.get("reg_url", "")
+            self.suno_prefix = self.config.get("suno_prefix", [])
             self.params_cache = ExpiredDict(500)
             # 初始化成功日志
 
@@ -88,86 +89,94 @@ class membercheck(Plugin):
             self.params_cache[user_id]['credit'] = 3
             logger.info('Added new user to params_cache. user id = ' + user_id)
 
+        success, send_url, is_vip, balance, balance_draw,balance_video,balance_gpt4,vip_time,vip_tip = self.getcredit(user_id)
+
+        if not success:
+            tip = f"💡服务器无法连接，请稍后再试"
+            reply = Reply(type=ReplyType.TEXT, content= tip)
+            e_context["reply"] = reply
+            e_context.action = EventAction.BREAK_PASS
+            return
+
         if e_context['context'].type == ContextType.TEXT:
-             if content.startswith(self.credit_prefix):
-
-                try:
-                    # 创建cursor以执行查询
-                    cursor = self.db.cursor()
-                    # ... 剩下的代码部分保持不变
-
-                    # SQL 查询语句
-                    sql = "SELECT balance, balance_draw, balance_video, balance_gpt4, vip_expire_time FROM fox_chatgpt_user WHERE openid_mp = %s"
-                    
-                    # 执行SQL语句
-                    cursor.execute(sql, (user_id,))
-                    logger.info("SQL查询执行成功")
-
-                    # 获取所有记录列表
-                    results = cursor.fetchall()
-                    send_url = False
-                    balance = 0
-                    balance_draw = 0
-                    balance_video = 0
-                    balance_gpt4 = 0
-                    vip_expire_time = 0
-                    is_vip = False
-                    vip_tip = '没有开通会员'
-                    vip_time = ''
-                    if results:
-                        for row in results:
-                            # 通过索引访问行中的数据
-                            balance = row[0]
-                            balance_draw = row[1]
-                            balance_video = row[2]
-                            balance_gpt4 = row[3]
-                            vip_expire_time = row[4]
-
-                            # 打印获得的值
-                            logger.info(f"balance = {balance}")
-                            logger.info(f"balance_draw = {balance_draw}")
-                            logger.info(f"balance_video = {balance_video}")
-                            logger.info(f"balance_gpt4 = {balance_gpt4}")
-                            if vip_expire_time > 0:
-                                # 将时间戳转换为 datetime 对象
-                                is_vip = True
-                                dt_object = datetime.fromtimestamp(vip_expire_time, tz=timezone.utc)
-                                vip_time = dt_object.strftime('%Y-%m-%d %H:%M:%S %Z')
-                                logger.info(dt_object.strftime('%Y-%m-%d %H:%M:%S %Z'))
-                    else:
-                        logger.info("没有查询到任何数据。")
-                        send_url = True
-
-                except Exception as e:
-                    logger.info(f"查询失败: {e}")
-
+             
+            if content.startswith(self.credit_prefix):   
                 if(is_vip):
                     vip_tip = f"您已经开通会员。会员到期时间为：{vip_time}"
+
                 if send_url:
-                    tip = f"💡没有查询到用户信息，请前往 https://starlinkai.top/web 注册，并使用微信登录"
+                    tip = f"💡没有查询到用户信息，请前往{self.reg_url}注册，并使用微信登录"
                     reply = Reply(type=ReplyType.TEXT, content= tip)
                     e_context["reply"] = reply
                     e_context.action = EventAction.BREAK_PASS
                 else:    
-                    tip = f"💡尊敬的用户，您目前的余额如下：\n普通对话条数：{balance}\n绘画额度：{balance_draw}\n视频余额:{balance_video}\n高级版剩余字数:{balance_gpt4}\n会员状态：{vip_tip}"
+                    tip = f"💡尊敬的用户，您目前的余额如下：\n✨普通对话条数：{balance}\n✨绘画额度：{balance_draw}\n✨视频余额：{balance_video}\n✨高级版剩余字数：{balance_gpt4}\n✨会员状态：{vip_tip}"
                     reply = Reply(type=ReplyType.TEXT, content= tip)
                     e_context["reply"] = reply
                     e_context.action = EventAction.BREAK_PASS
-            # if user_id in self.params_cache and isgroup:
-            #     match_command = False
-            #     for command in self.exclude_commands:
-            #         if content.startswith(command):
-            #             logger.info("检测到特定指令, 不计算credit")
-            #             match_command = True                            
 
-            #     if self.params_cache[user_id]['credit'] > 0:
-            #         if not match_command:
-            #             self.params_cache[user_id]['credit'] -= 1
-            #             logger.info(f"当前用户id: {user_id} \n当前credit: {self.params_cache[user_id]['credit']}")
-            #         e_context.action = EventAction.CONTINUE  # 事件继续，交付给下个插件或默认逻辑
-            #     else:
-            #         error_tip = f"尊敬的用户，您好！我是AI小助理微米精灵，您的免费体验次数已达到上限，如果您对我的服务满意的话，可以开通如下服务：\n1️⃣加好友：不限次数的AI专属服务。\n2️⃣开群聊：让AI助理为你的群做推广。\n3️⃣定制化：创建定制自己品牌的AI助理。\n欢迎联系微米客服开通服务。"
-            #         reply = Reply(type=ReplyType.TEXT, content= error_tip)
-            #         e_context["reply"] = reply
-            #         e_context.action = EventAction.BREAK_PASS
+            elif any(content.startswith(prefix) for prefix in self.suno_prefix):
+                if(is_vip):
+                    e_context.action = EventAction.CONTINUE
+                else:
+                    tip = f"💡非常抱歉，此功能为会员功能，请加入会员再重试，感谢您的支持！"
+                    reply = Reply(type=ReplyType.TEXT, content= tip)
+                    e_context["reply"] = reply
+                    e_context.action = EventAction.BREAK_PASS
+
+    def getcredit(self, user_id):
+        try:
+            # 创建cursor以执行查询
+            cursor = self.db.cursor()
+            # ... 剩下的代码部分保持不变
+
+            # SQL 查询语句
+            sql = "SELECT balance, balance_draw, balance_video, balance_gpt4, vip_expire_time FROM fox_chatgpt_user WHERE openid_mp = %s"
             
+            # 执行SQL语句
+            cursor.execute(sql, (user_id,))
+            logger.info("SQL查询执行成功")
+
+            # 获取所有记录列表
+            results = cursor.fetchall()
+            send_url = False
+            balance = 0
+            balance_draw = 0
+            balance_video = 0
+            balance_gpt4 = 0
+            vip_expire_time = 0
+            is_vip = False
+            vip_tip = '没有开通会员'
+            vip_time = ''
+            if results:
+                for row in results:
+                    # 通过索引访问行中的数据
+                    balance = row[0]
+                    balance_draw = row[1]
+                    balance_video = row[2]
+                    balance_gpt4 = row[3]
+                    vip_expire_time = row[4]
+
+                    # 打印获得的值
+                    logger.info(f"balance = {balance}")
+                    logger.info(f"balance_draw = {balance_draw}")
+                    logger.info(f"balance_video = {balance_video}")
+                    logger.info(f"balance_gpt4 = {balance_gpt4}")
+                    if vip_expire_time > 0:
+                        # 将时间戳转换为 datetime 对象
+                        is_vip = True
+                        dt_object = datetime.fromtimestamp(vip_expire_time, tz=timezone.utc)
+                        vip_time = dt_object.strftime('%Y-%m-%d %H:%M:%S')
+                        logger.info(dt_object.strftime('%Y-%m-%d %H:%M:%S'))
+            else:
+                logger.info("没有查询到任何数据。")
+                send_url = True
+            success = True
+
+        except Exception as e:
+            logger.info(f"查询失败: {e}")
+            success = False
+    
+        return success, send_url, is_vip, balance, balance_draw,balance_video,balance_gpt4,vip_time,vip_tip
+
+           
